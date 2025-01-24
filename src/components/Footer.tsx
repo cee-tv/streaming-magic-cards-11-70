08f8f43dac400da4a87e1970f3d8a3667b3a8296
Jayce <kendump666@gmail.com>
2025-01-24 08:26:23 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,16 @@ export const Footer = () => {
       <div className="container mx-auto px-4">
         {/* Social Media Icons */}
         <div className="flex gap-6 mb-8">
-          <a href="#" className="hover:text-white transition-colors">
+          <a href="https://poawooptugroo.com/4/8835890" className="hover:text-white transition-colors">
             <Facebook className="w-6 h-6" />
           </a>
-          <a href="#" className="hover:text-white transition-colors">
+          <a href="https://poawooptugroo.com/4/8841984" className="hover:text-white transition-colors">
             <Instagram className="w-6 h-6" />
           </a>
-          <a href="#" className="hover:text-white transition-colors">
+          <a href="https://poawooptugroo.com/4/8841985" className="hover:text-white transition-colors">
             <Twitter className="w-6 h-6" />
           </a>
-          <a href="#" className="hover:text-white transition-colors">
+          <a href="https://poawooptugroo.com/4/8841986" className="hover:text-white transition-colors">
             <Youtube className="w-6 h-6" />
           </a>
         </div>
@@ -24,24 +24,24 @@ export const Footer = () => {
         {/* Footer Links */}
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
           <div className="space-y-4">
-            <a href="#" className="block hover:text-white">Audio Description</a>
-            <a href="#" className="block hover:text-white">Gift Cards</a>
-            <a href="#" className="block hover:text-white">Investor Relations</a>
+            <a href="https://poawooptugroo.com/4/8835890" className="block hover:text-white">Audio Description</a>
+            <a href="https://poawooptugroo.com/4/8841984" className="block hover:text-white">Gift Cards</a>
+            <a href="https://poawooptugroo.com/4/8841985" className="block hover:text-white">Investor Relations</a>
           </div>
           <div className="space-y-4">
-            <a href="#" className="block hover:text-white">Terms of Use</a>
-            <a href="#" className="block hover:text-white">Legal Notices</a>
-            <a href="#" className="block hover:text-white">Corporate Information</a>
+            <a href="https://poawooptugroo.com/4/8835890" className="block hover:text-white">Terms of Use</a>
+            <a href="https://poawooptugroo.com/4/8841984" className="block hover:text-white">Legal Notices</a>
+            <a href="https://poawooptugroo.com/4/8841985" className="block hover:text-white">Corporate Information</a>
           </div>
           <div className="space-y-4">
-            <a href="#" className="block hover:text-white">Help Center</a>
-            <a href="#" className="block hover:text-white">Media Center</a>
-            <a href="#" className="block hover:text-white">Jobs</a>
+            <a href="https://poawooptugroo.com/4/8841985" className="block hover:text-white">Help Center</a>
+            <a href="https://poawooptugroo.com/4/8841984" className="block hover:text-white">Media Center</a>
+            <a href="https://poawooptugroo.com/4/8835890" className="block hover:text-white">Jobs</a>
           </div>
           <div className="space-y-4">
-            <a href="#" className="block hover:text-white">Privacy</a>
-            <a href="#" className="block hover:text-white">Cookie Preferences</a>
-            <a href="#" className="block hover:text-white">Contact Us</a>
+            <a href="https://poawooptugroo.com/4/8835890" className="block hover:text-white">Privacy</a>
+            <a href="https://poawooptugroo.com/4/8841984" className="block hover:text-white">Cookie Preferences</a>
+            <a href="https://poawooptugroo.com/4/8841985" className="block hover:text-white">Contact Us</a>
           </div>
         </div>
 
